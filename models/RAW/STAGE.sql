f8bdf5babfecc126f624876bb9b850f8befeab6e
Ashik Roshan I <ashikroshan261@gmail.com>
2024-03-15 07:41:42 +0000

    Changes commit

---
+++ models/RAW/STAGE.sql
@@ -0,0 +1,10 @@
+SELECT 
+    c.customer_id, 
+    c.customer_name, 
+    c.email, 
+    o.order_id, 
+    o.order_date, 
+    o.total_amount
+FROM {{ ref('RAW_CUSTOMERS') }} as c
+JOIN  {{ ref('RAW_ORDERS') }} as o 
+ON c.customer_id = o.customer_id
