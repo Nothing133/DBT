ac0e3eee5a6c0ff5a19c2536c3e90e1831030c94
Ashik Roshan I <ashikroshan261@gmail.com>
2024-06-07 17:01:15 +0000

    Deletion

--- models/sources/account_account_financial_report_model.sql
+++
@@ -1,8 +0,0 @@
--- models/account_account_financial_report_model.sql
-
-{{ config(materialized='table') }}
-
-select
-    REPORT_LINE_ID as changed_report_line_id,
-    ACCOUNT_ID as changed_account_id
-from {{ source('PSQLSOURCE', 'ACCOUNT_ACCOUNT_FINANCIAL_REPORT') }}
