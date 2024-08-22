6400b45cb81e3afdb7e0063a32001a952285cbd0
Ashik Roshan I <ashikroshan261@gmail.com>
2024-08-22 14:39:43 +0000

    CHANGES

--- tests/test_not_null_custom.sql
+++ tests/test_not_null_custom.sql
@@ -1,5 +1,30 @@
+-- tests/test_not_null_custom.sql
+
 {% set model = ref('ODS_ES_BYOD_PROJ_HOUR_COST_PRICE') %}
-{% set columns = ['DEFINITIONGROUP', 'EXECUTIONID'] %}
+{% set columns = [
+    'DEFINITIONGROUP',
+    'EXECUTIONID',
+    'ISSELECTED',
+    'TRANSFERSTATUS',
+    'CATEGORYID',
+    'COSTPRICE',
+    'EFFECTIVELABORRATE',
+    'ISRESOURCERATE',
+    'PROJECTID',
+    'CUSTOMERACCOUNT',
+    'PRICEGROUPID',
+    'PROJECTCONTRACTID',
+    'RESOURCECATEGORY',
+    'TRANSDATE',
+    'RESOURCEID',
+    'RESOURCECATEGORYID',
+    'RESOURCECOMPANYID',
+    'PARTITION',
+    'DATAAREAID',
+    'SYNCSTARTDATETIME',
+    'RECID',
+    'SF_INSERT_TIMESTAMP'
+] %}
 
 {% for column_name in columns %}
     {% set result = run_query(test_not_null_custom(model, [column_name])) %}
@@ -9,5 +34,3 @@
         select '{{ column_name }}' as column_name, 'PASS' as result
     {% endif %}
 {% endfor %}
- DEFINITIONGROUP 
-	
