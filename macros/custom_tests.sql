6f1099e3c326536c25ead5d2899183d209fb2bcd
Ashik Roshan I <ashikroshan261@gmail.com>
2024-08-22 11:19:20 +0000

    commit change

--- macros/custom_tests.sql
+++ macros/custom_tests.sql
@@ -1,21 +1,19 @@
-{% macro all_columns_not_null(model, exclude_column_list=[]) %}
-    {%- if execute -%}
-    {%- set model_columns = adapter.get_columns_in_relation(model) -%}
-
-    -- Initialize an empty string to hold the generated SQL
-    {%- set sql_string = "" -%}
-
-    {%- for col in model_columns if col.column|upper not in exclude_column_list|map('upper') %}
-        -- Add SQL for checking null values in the current column
-        {%- set sql_string = sql_string + "select count(*) as c from (select " + col.column + " from " + model + " where " + col.column + " is null) having c > 0" -%}
-        
-        -- Add UNION ALL unless it's the last column
-        {%- if not loop.last %}
-        {%- set sql_string = sql_string + " union all " -%}
-        {%- endif %}
-    {%- endfor -%}
-
-    -- Return the complete SQL string
-    {{ sql_string }}
-    {%- endif -%}
+{% macro check_columns_not_null(model) %}
+  {% set column_list = meta.get('column_list', []) %}
+  
+  {% if column_list|length == 0 %}
+    {% do log("No columns defined in schema.yml", info=True) %}
+    SELECT 1 AS status WHERE FALSE
+  {% else %}
+    SELECT *
+    FROM {{ ref(model) }}
+    WHERE NOT (
+      {% for column in column_list %}
+        {{ column }} IS NOT NULL
+        {% if not loop.last %}
+          AND
+        {% endif %}
+      {% endfor %}
+    )
+  {% endif %}
 {% endmacro %}
---
+++ tests/check_columns_not_null.sql
@@ -0,0 +1,1 @@
+{{ check_columns_not_null('ODS_ES_BYOD_PROJ_HOUR_COST_PRICE') }}
