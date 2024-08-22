f8d1a326b50f751e977a8264a414b223ae2b426c
Ashik Roshan I <ashikroshan261@gmail.com>
2024-08-22 18:08:53 +0000

    changes

--- macros/all_columns_not_null.sql
+++ macros/all_columns_not_null.sql
@@ -7,7 +7,7 @@
 
     {%- for col in model_columns if col.column|upper not in exclude_column_list|map('upper') %}
         -- For each column not excluded, add SQL to check for NULL values
-        {%- set sql_string = sql_string + "SELECT COUNT(*) AS c FROM (SELECT " + col.column + " FROM " + model + " WHERE " + col.column + " IS NULL) HAVING c > 0" -%}
+        {%- set sql_string = "SELECT COUNT(*) AS c FROM (SELECT " + col.column + " FROM " + model + " WHERE " + col.column + " IS NULL) HAVING c > 0" -%}
         
         -- Add UNION ALL between different columns' queries unless it's the last column
         {%- if not loop.last %}
