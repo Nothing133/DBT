c6619169cf18e3bf0913de387a568ab1e267c6cf
Ashik Roshan I <ashikroshan261@gmail.com>
2024-08-22 18:34:31 +0000

    save

--- macros/all_columns_not_null.sql
+++ macros/all_columns_not_null.sql
@@ -1,13 +1,10 @@
-{% macro all_columns_not_null(model, exclude_column_list) %}
+{% macro test_columns_not_null(model, column_list) %}
     {%- if execute -%}
-    {%- set model_columns = adapter.get_columns_in_relation(model) -%}
-
-    -- Initialize an empty string to hold the generated SQL
     {%- set sql_string = "" -%}
-
-    {%- for col in model_columns if col.column|upper not in exclude_column_list|map('upper') %}
-        -- For each column not excluded, add SQL to check for NULL values
-        {%- set sql_string = "SELECT COUNT(*) AS c FROM (SELECT " + col.column + " FROM " + model + " WHERE " + col.column + " IS NULL) HAVING c > 0" -%}
+    
+    {%- for col in column_list %}
+        -- Add SQL to check for NULL values in each specified column
+        {%- set sql_string = sql_string + "SELECT COUNT(*) AS c, '" + col + "' AS column_name FROM " + model + " WHERE " + col + " IS NULL" -%}
         
         -- Add UNION ALL between different columns' queries unless it's the last column
         {%- if not loop.last %}
@@ -15,7 +12,7 @@
         {%- endif %}
     {%- endfor -%}
 
-    -- Return the complete SQL string for the test
-    {{ sql_string }}
+    -- Add a HAVING clause to check if any column has NULL values
+    {{ sql_string + " HAVING SUM(c) > 0" }}
     {%- endif -%}
 {% endmacro %}
