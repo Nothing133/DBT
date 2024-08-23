2d82a398d830b541bcdab66c5a4dbafd9b93dc83
Ashik Roshan I <ashikroshan261@gmail.com>
2024-08-23 04:59:58 +0000

    changes

--- macros/all_columns_not_null.sql
+++ macros/all_columns_not_null.sql
@@ -1,21 +1,17 @@
-{% macro all_columns_not_null(model, exclude_column_list) %}
-    {%- if execute -%}
-    {%- set model_columns = adapter.get_columns_in_relation(model) -%}
+{% test test_columns_not_null(model, column_list) %}
+{%- if execute -%}
+{%- set model_columns = adapter.get_columns_in_relation(model) -%}
 
-    -- Initialize an empty string to hold the generated SQL
-    {%- set sql_string = "" -%}
-
-        {%- for col in model_columns if col.column|upper not in exclude_column_list|map('upper') %}
-        -- Add SQL for checking null values in the current column
-        {%- set sql_string = sql_string + "select count(*) as c from (select " + col.column + " from " + model + " where " + col.column + " is null) having c > 0)" -%}
-
-        -- Add UNION ALL unless it's the last column
-        {%- if not loop.last %}
-        {%- set sql_string = sql_string + " union all " -%}
-        {%- endif %}
-    {%- endfor -%}
-
-    -- Return the complete SQL string
-    {{ sql_string }}
-    {%- endif -%}
-{% endmacro %}
+{%- for col in model_columns if col.column|upper in column_list|upper %}
+    select count(*) as c from (
+        select {{ col.column }}
+        from {{ model }}
+        where {{ col.column }} is null
+    )
+    having c > 0
+    {%- if not loop.last %}
+    union all
+    {%- endif %}
+{%- endfor -%}
+{%- endif -%}
+{% endtest %}
--- tests/all_columns_not_null.sql
+++
@@ -1,3 +0,0 @@
-{% test all_columns_not_null(model, exclude_column_list) %}
-    {{ all_columns_not_null(model, exclude_column_list) }}
-{% endtest %}
