48636102a6c28d780993063f1db6d33b064c1a4d
Ashik Roshan I <ashikroshan261@gmail.com>
2024-11-21 14:41:09 +0000

    change

--- macros/purge_full_load_staging.sql
+++ macros/purge_full_load_staging.sql
@@ -1,7 +1,9 @@
--- macros/purge_full_load_staging.sql
 {% macro purge_full_load_staging(source_name, table_name, dedup_keys) %}
+    -- Join dedup_keys for use in SQL
     {% set dedup_columns_sql = dedup_keys | join(', ') %}
-    delete from {{ source(source_name, table_name) }}
+
+    -- Perform deduplication
+    delete from {{ source('demo_staging', table_name) }}  -- Use the new source name
     where ({{ dedup_columns_sql }}, SF_INSERT_TIMESTAMP) in 
     (
         select {{ dedup_columns_sql }}, SF_INSERT_TIMESTAMP
@@ -10,7 +12,7 @@
             select *, 
                    row_number() over (partition by {{ dedup_columns_sql }} 
                                       order by SF_INSERT_TIMESTAMP desc) as row_num
-            from {{ source(source_name, table_name) }}
+            from {{ source('demo_staging', table_name) }}  -- Updated source name
         ) t
         where t.row_num > 1
     );
