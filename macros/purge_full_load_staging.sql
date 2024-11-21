87863e4e860a238e8b7d23d71169dcd9c477be32
Ashik Roshan I <ashikroshan261@gmail.com>
2024-11-21 14:28:59 +0000

    change

--- macros/purge_full_load_staging.sql
+++ macros/purge_full_load_staging.sql
@@ -1,32 +1,17 @@
-{% macro purge_full_load_staging(source_name, table_name) %}
-    -- Metadata column to be excluded 
-    {% set metadata_column = ['SF_INSERT_TIMESTAMP'] %}
-
-    -- Get all columns from the table
-    {% set all_columns = adapter.get_columns_in_relation(source(source_name, table_name)) %}
-
-    -- Empty list to store columns for deduplication
-    {% set dedup_columns = [] %}
-
-    -- Looping all columns and adding columns that are not in metadata_column
-    {% for col in all_columns %}
-        {% if col.name not in metadata_column %}
-            {% do dedup_columns.append(col.name) %}
-        {% endif %}
-    {% endfor %}
-
-    {% set dedup_columns = dedup_columns | join(', ') %}
-
+-- macros/purge_full_load_staging.sql
+{% macro purge_full_load_staging(source_name, table_name, dedup_keys) %}
+    {% set dedup_columns_sql = dedup_keys | join(', ') %}
     delete from {{ source(source_name, table_name) }}
-    where ({{ dedup_columns }}, SF_INSERT_TIMESTAMP) in 
+    where ({{ dedup_columns_sql }}, SF_INSERT_TIMESTAMP) in 
     (
-        select {{ dedup_columns }}, SF_INSERT_TIMESTAMP
+        select {{ dedup_columns_sql }}, SF_INSERT_TIMESTAMP
         from 
         (
-            select *,
-            row_number() over (partition by {{ dedup_columns }} order by SF_INSERT_TIMESTAMP desc) as row_num
+            select *, 
+                   row_number() over (partition by {{ dedup_columns_sql }} 
+                                      order by SF_INSERT_TIMESTAMP desc) as row_num
             from {{ source(source_name, table_name) }}
         ) t
         where t.row_num > 1
-    )
+    );
 {% endmacro %}
--- models/modeldemo.sql
+++ models/modeldemo.sql
@@ -1,1 +1,12 @@
-{{purge_full_load_staging('staging','STG_ES_AESOP_REF_WASTE_TYPE')}}
+{{
+    config(
+        post_hook="purge_full_load_staging(
+            'staging', 
+            'STG_ES_AESOP_REF_WASTE_TYPE'
+        )"
+    )
+}}
+
+-- Model logic
+select *
+from {{ source('staging', 'STG_ES_AESOP_REF_WASTE_TYPE') }}
